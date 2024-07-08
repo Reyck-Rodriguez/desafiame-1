18a20aa553611858cc8a01f6396483cd2419fdcb
Reyck-Rodriguez <y_unior16@hotmail.com>
2024-07-08 14:27:41 -0500

    actualizacion de esta monda
    aqui se ve mas bonito y cambiamos el tex area de rectangulo para que me mostre lo que el usuario ingresa

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-let TextoCifrado=0;
-let TextoUser=[];
+// Añade este código al final del archivo app.js
 
-function name(texuser) {
-    TextoUser=document.querySelector(texuser);
-    alert(TextoUser);
-}
+// Obtiene los elementos input y textarea
+let texuserInput = document.getElementById('texuser');
+let textAreaOutput = document.getElementById('text-area');
+
+// Añade un evento de escucha al input
+texuserInput.addEventListener('input', (event) => {
+  // Obtiene el valor del input
+  let inputValue = event.target.value;
+
+  // Actulizar el valor del textarea
+  textAreaOutput.value = inputValue;
+});
+
+function encrypto(texuser) {
+  
+    
+  }
+    
