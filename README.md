7a0ac406b22219c7bdcd0c35b7c9e9d3404571a8
Reyck-Rodriguez <y_unior16@hotmail.com>
2024-07-10 07:33:51 -0500

    segunda version
    ya mejor acomodado el layu e implementadas las funciones encriptar, desencriptar, copiar y notificación

---
+++ README.md
@@ -0,0 +1,16 @@
+## Decodificador y codificador de texto
+
+que realmente es un cambiador de letras cambia las vocales y las vuelve a reajustar
+
+Esta aplicacion decodifica texto y lo codifica con el metodo dado a continuacion:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+
+
+autor Junior Ivan Rodriguez 
+
+por alura latam y oracle one nex education =D
--- app.js
+++ app.js
@@ -1,20 +1,65 @@
-// Añade este código al final del archivo app.js
+const decodificado = document.getElementById("decodificar");
+const textarea = document.getElementById("texto_decode");
+const btnEncriptar = document.getElementById("encriptar_btn");
+const btnDesencriptar = document.getElementById("desencriptar_btn");
 
-// Obtiene los elementos input y textarea
-let texuserInput = document.getElementById('texuser');
-let textAreaOutput = document.getElementById('text-area');
+function encriptar(msg){
+    let newMsg=msg.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+    console.log(newMsg);
+    return newMsg;
+}
 
-// Añade un evento de escucha al input
-texuserInput.addEventListener('input', (event) => {
-  // Obtiene el valor del input
-  let inputValue = event.target.value;
+function desencriptar(msg){
+    let newMsg=msg.replaceAll('ai', 'a').replaceAll('enter', 'e').replaceAll('ufat', 'u').replaceAll('imes', 'i').replaceAll('ober', 'o');
+    console.log(newMsg);
+    return newMsg;
+}
 
-  // Actulizar el valor del textarea
-  textAreaOutput.value = inputValue;
+function mostrarResultado(modo){
+    if(textarea.value!=="")
+        {
+            decodificado.innerHTML=`
+            <div class="textoD" id="textoD">
+                <p>${modo==="encriptar" ? encriptar(textarea.value) : desencriptar(textarea.value)}</p>
+            </div>
+            <button class="copiar" id="copiar">Copiar</button>`;
+            
+            
+            const texto = document.getElementById("textoD").innerText;
+            const btnCopiar = document.getElementById("copiar");
+            
+            const copiarContenido = async () => {
+                try {
+                await navigator.clipboard.writeText(texto);
+                } catch (err) {
+                console.error('Error al copiar: ', err);
+                }
+            }
+    
+            btnCopiar.addEventListener("click", e => {
+                copiarContenido();
+                document.querySelector('.notification_copy').classList.toggle('inactive');
+                setTimeout(()=> document.querySelector('.notification_copy').classList.toggle('inactive'), 2000)
+            })
+        }
+}
+
+btnEncriptar.addEventListener('click', e => {
+    mostrarResultado('encriptar');
 });
 
-function encrypto(texuser) {
+btnDesencriptar.addEventListener('click', e => {
+    mostrarResultado('desencriptar');
+})
   
-    
+  function validarCaracteres(texto) {
+    const regex = /[0-9\u0300-\u036f\u1E00-\u1EFF\u2000-\u303F\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF\u3400-\u4DBF\u4E00-\u9FFF\uAC00-\uD7AF\uF900-\uFAFF\äÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙ]/;
+    return regex.test(texto);
   }
-    
+  
+  // Uso de la función
+  textarea.addEventListener('keydown', e => {
+    if (validarCaracteres(e.key)) {
+      e.preventDefault();
+    }
+  });
